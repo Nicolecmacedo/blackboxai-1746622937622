8d40865388bc3d72e5f1bcabdf293f7abd16557a
Nicolecmacedo <niikcarvalho@gmail.com>
2025-05-07 10:02:23 -0300

    Add mundos-do-saber/js/data/questions.js 1746622942706

---
+++ mundos-do-saber/js/data/questions.js
@@ -0,0 +1,40 @@
+const questions = {
+  alfabetizacao: [
+    {
+      text: "Qual palavra começa com o som /m/?",
+      options: ["Macaco", "Tigre", "Cavalo"],
+      correct: "Macaco"
+    },
+    {
+      text: "Complete: C__SA",
+      options: ["A", "O", "E"],
+      correct: "A"
+    }
+  ],
+  matematica: [
+    {
+      text: "Quanto é 2 + 3?",
+      options: ["4", "5", "6"],
+      correct: "5"
+    },
+    {
+      text: "Quanto é 10 - 4?",
+      options: ["6", "5", "4"],
+      correct: "6"
+    }
+  ],
+  cognicao: [
+    {
+      text: "Quem usa jaleco branco?",
+      options: ["Médico", "Professor", "Padeiro"],
+      correct: "Médico"
+    },
+    {
+      text: "Onde guardamos comida gelada?",
+      options: ["Freezer", "Fogão", "Armário"],
+      correct: "Freezer"
+    }
+  ]
+};
+
+export default questions;
